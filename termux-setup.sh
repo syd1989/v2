2108d76645b525f88898ce1b685f03b5f688058c
syd1989 <99587555+syd1989@users.noreply.github.com>
2022-02-13 22:11:31 +0800

    Add files via upload

---
+++ termux-setup.sh
@@ -0,0 +1,23 @@
+export URL=https://ghproxy.com/https://github.com/egandro/docker-qemu-arm
+export BRANCH=master
+
+pkg install -y expect wget qemu-utils qemu-common qemu-system-x86_64-headless openssh
+
+mkdir -p alpine
+cd alpine
+
+if [ ! -f "config.env" ]; then
+    # get the default config file
+    wget -q -c -t0 ${URL}/${BRANCH}/config.env
+fi
+
+# URL / BRANCH might be overwritten here
+. config.env
+
+wget -q -c -t0 ${URL}/${BRANCH}/answerfile
+wget -q -c -t0 ${URL}/${BRANCH}/ssh2qemu.sh
+chmod +x ./ssh2qemu.sh
+wget -q -c -t0 ${URL}/${BRANCH}/startqemu.sh
+chmod +x ./startqemu.sh
+wget -q -c -t0 ${URL}/${BRANCH}/installqemu.expect
+expect -f installqemu.expect
