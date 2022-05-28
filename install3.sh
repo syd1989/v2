f4118a615c0ad8e3c790e4361a18d062db605f68
syd1989 <99587555+syd1989@users.noreply.github.com>
2022-05-28 21:46:50 +0800

    Add files via upload

--- install3.sh
+++ install3.sh
@@ -2,7 +2,7 @@ pkg install vim curl wget git unzip unrar
 pkg  nodejs
 npm install
 
-git clone https://ghproxy.com/https://github.com/abinnz/jdlite-server.git
+git clone https://gh.fakev.cn/abinnz/jdlite-server.git
 
 cd ~
 wget -N "https://ghproxy.com/https://github.com/syd1989/v2/blob/main/jdlog.sh"
